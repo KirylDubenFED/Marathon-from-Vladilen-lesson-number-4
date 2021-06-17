02ffe15920c1e789beef7fc24fe56ca5fdb8ca5d
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-06-18 00:17:01 +0300

    add code script

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const board = document.querySelector('.board')
+const board = document.querySelector('#board')
 const SQUARES_NUMBER = 700
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
-    
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () =>
+    setColor(square)
+  )
+
+  board.append(square)
+}
+
+function setColor(element) {
+  element.style.backgroundColor = 'red'
 }
