9e6da8f60a4dd2c325a1aac20bcad1d448e41e63
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-06-19 13:57:05 +0300

    finish game

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 const board = document.querySelector('#board')
-const colors = ['#FF1493', '#00FF00', '#0000FF', 
-'#008000', '#008B8B', '#FF0000', '#00FFFF', '#FF4500']
+const colors = ['#FF1493', '#00FF00', '#0000FF',
+  '#008000', '#008B8B', '#FF0000', '#00FFFF', '#FF4500']
 const SQUARES_NUMBER = 600
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
-
-  square.addEventListener('mouseover', () =>
-    setColor(square)
-  )
-
-  square.addEventListener('mouseleave', () =>
-    removeColor(square)
-  )
-
+  square.addEventListener('mouseover', setColor)
+  square.addEventListener('mouseleave', removeColor)
   board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target
   const color = getRandomColor()
+
   element.style.backgroundColor = color
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target
   element.style.backgroundColor = '#1d1d1d'
   element.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor() {
- return colors[Math.floor(Math.random() * colors.length)]
+  return colors[Math.floor(Math.random() * colors.length)]
 }
